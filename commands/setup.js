a5c0e225fd6b4d6222158e96cdb9a5c3b7039d7c
Chris Parnin <chris.parnin@gmail.com>
2020-02-02 16:12:49 -0500

    Print error, if any.

--- commands/setup.js
+++ commands/setup.js
@@ -35,20 +35,20 @@ async function run(privateKey) {
 
     console.log(chalk.blueBright('Provisioning configuration server...'));
     let result = child.spawnSync(`bakerx`, `run ansible-srv bionic --ip 192.168.33.10 --sync`.split(' '), {stdio: 'inherit'} );
-    if( result.error ) { process.exit( result.status ); }
+    if( result.error ) { console.log(result.error); process.exit( result.status ); }
 
     console.log(chalk.blueBright('Provisioning mattermost server...'));
     result = child.spawnSync(`bakerx`, `run mattermost-srv bionic --ip 192.168.33.80`.split(' '), {stdio: 'inherit'} );
-    if( result.error ) { process.exit( result.status ); }
+    if( result.error ) { console.log(result.error); process.exit( result.status ); }
 
     console.log(chalk.blueBright('Installing privateKey on configuration server'));
     let identifyFile = privateKey || path.join(os.homedir(), '.bakerx', 'insecure_private_key');
     result = scpSync (identifyFile, 'vagrant@192.168.33.10:/home/vagrant/.ssh/mm_rsa');
-    if( result.error ) { process.exit( result.status ); }
+    if( result.error ) { console.log(result.error); process.exit( result.status ); }
 
     console.log(chalk.blueBright('Running init script...'));
     result = sshSync('/bakerx/cm/server-init.sh', 'vagrant@192.168.33.10');
-    if( result.error ) { process.exit( result.status ); }
+    if( result.error ) { console.log(result.error); process.exit( result.status ); }
 
 
 
